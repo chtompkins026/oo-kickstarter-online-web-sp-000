c284c0dc1a156a44157ba22cdaf576dddaa2dfcf
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-25 15:41:59 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,8 +6,8 @@ class Backer
     @backed_projects = []
   end
   
-  def backed_projects(projects)
-    @backed_projects << projects 
-  end 
-
-end 
+ def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+end
